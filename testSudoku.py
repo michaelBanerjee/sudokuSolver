d472ba016b8268c02e4eee6e457549f9603f15e4
michaelbanerjee <michaelbanerjee10@gmail.com>
2023-11-02 17:41:21 -0400

    feat: made board a parameter for sudokuGame

--- testSudoku.py
+++ testSudoku.py
@@ -2,11 +2,22 @@ import unittest
 from sudoku import sudokuGame
 
 class testSudoku(unittest.TestCase):
+    board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
     
-    game = sudokuGame()
+    game = sudokuGame(board)
     
-    def testGetSubGrid(self):
-        self.assertEqual([5, 3, 0, 6, 0, 0, 0, 9, 8],self.game.getSubgrid())
+    def testGetThreeByThreeArea(self):
+        self.assertEqual([5, 3, 0, 6, 0, 0, 0, 9, 8],self.game.getThreeByThreeArea(0,0))
 
 if __name__ == "__main__":
     unittest.main()
