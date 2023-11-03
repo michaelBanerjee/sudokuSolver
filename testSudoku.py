729cf7af169f60adc2959596690e6461177e0a66
michaelbanerjee <michaelbanerjee10@gmail.com>
2023-11-03 13:24:52 -0400

    getSubgrid() exceptions handled and tests implemented

--- testSudoku.py
+++ testSudoku.py
@@ -14,6 +14,17 @@ class testSudoku(unittest.TestCase):
             [0, 0, 0, 0, 8, 0, 0, 7, 9]
         ]
     game = sudokuGame(board)
+
+    def testGetSubgrid(self):
+        self.assertEquals([0, 7, 0, 1, 9, 5, 0, 0, 0], self.game.getSubgrid(1))
+        self.assertEqual([0, 0, 0, 0, 0, 0, 0, 6, 0], self.game.getSubgrid(2))
+        self.assertEqual([8 ,0 ,0 ,4 ,0 ,0 ,7 ,0 ,0], self.game.getSubgrid(3))
+        self.assertEqual([0 ,6 ,0 ,8 ,0 ,3 ,0 ,2 ,0], self.game.getSubgrid(4))
+        self.assertEqual([0 ,0 ,0 ,4 ,1 ,9 ,0 ,8 ,0], self.game.getSubgrid(7))
+        with self.assertRaises(IndexError):
+            self.game.getSubgrid(-1)
+            self.game.getSubgrid(9)
+
     
     def testGetThreeByThreeArea(self):
         self.assertEqual([5, 3, 0, 6, 0, 0, 0, 9, 8], self.game.getThreeByThreeArea(0,0))
@@ -25,12 +36,7 @@ class testSudoku(unittest.TestCase):
             self.game.getThreeByThreeArea(7,0)
             self.game.getThreeByThreeArea(0,7)
     
-    def testGetSubgrid(self):
-        self.assertEquals([0, 7, 0, 1, 9, 5, 0, 0, 0], self.game.getSubgrid(1))
-        self.assertEqual([0, 0, 0, 0, 0, 0, 0, 6, 0], self.game.getSubgrid(2))
-        self.assertEqual([8 ,0 ,0 ,4 ,0 ,0 ,7 ,0 ,0], self.game.getSubgrid(3))
-        self.assertEqual([0 ,6 ,0 ,8 ,0 ,3 ,0 ,2 ,0], self.game.getSubgrid(4))
-        self.assertEqual([0 ,0 ,0 ,4 ,1 ,9 ,0 ,8 ,0], self.game.getSubgrid(7))
+    
 
 
             
